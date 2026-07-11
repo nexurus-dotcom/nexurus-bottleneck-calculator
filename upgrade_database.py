a2f28cdcd99f8883d4ad29667f2ba6ef72a628c8
Thomas Smith <nexuruscs@gmail.com>
2026-07-11 17:10:04 -0400

    Add CPU upgrade recommendations

--- upgrade_database.py
+++ upgrade_database.py
@@ -1,3 +1,67 @@
+cpu_upgrades = {
+    "Ryzen 5 3600": [
+        "Ryzen 5 5600",
+        "Ryzen 7 5700X3D",
+        "Ryzen 7 5800X3D"
+    ],
+
+    "Ryzen 5 5600": [
+        "Ryzen 7 5700X3D",
+        "Ryzen 7 5800X3D",
+        "Ryzen 7 7800X3D"
+    ],
+
+    "Ryzen 5 7600": [
+        "Ryzen 7 7700X",
+        "Ryzen 7 7800X3D",
+        "Ryzen 9 7900X3D"
+    ],
+
+    "Ryzen 7 5800X": [
+        "Ryzen 7 5800X3D",
+        "Ryzen 7 7800X3D"
+    ],
+
+    "Ryzen 7 5800X3D": [
+        "Ryzen 7 7800X3D",
+        "Ryzen 7 9800X3D"
+    ],
+
+    "Ryzen 7 7700X": [
+        "Ryzen 7 7800X3D",
+        "Ryzen 7 9800X3D",
+        "Ryzen 9 9900X3D"
+    ],
+
+    "Ryzen 7 7800X3D": [
+        "Ryzen 7 9800X3D",
+        "Ryzen 9 9900X3D"
+    ],
+
+    "Core i5-12400F": [
+        "Core i5-13600K",
+        "Core i5-14600K",
+        "Core i7-14700K"
+    ],
+
+    "Core i5-12600K": [
+        "Core i5-14600K",
+        "Core i7-14700K",
+        "Core i7-14700KF"
+    ],
+
+    "Core i5-13600K": [
+        "Core i7-14700K",
+        "Core i7-14700KF",
+        "Core i9-14900K"
+    ],
+
+    "Core i7-13700K": [
+        "Core i7-14700K",
+        "Core i9-14900K"
+    ]
+}
+
 gpu_upgrades = {
     "RTX 3060": [
         "NVIDIA RTX 4060",
