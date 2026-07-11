6b2662f7c164eee360ad9f86f8e6bbd88db6b9b5
nexurus-dotcom <nexuruscs@gmail.com>
2026-07-11 16:54:31 -0400

    Update upgrade_database.py

--- upgrade_database.py
+++ upgrade_database.py
@@ -1,19 +1,25 @@
 gpu_upgrades = {
-
-    "NVIDIA RTX 4060": [
+    "RTX 4060": [
         "NVIDIA RTX 5070",
         "NVIDIA RTX 5070 Ti",
         "AMD RX 9070 XT"
     ],
 
-    "NVIDIA RTX 4060 Ti": [
-        "NVIDIA RTX 5070",
-        "AMD RX 9070"
+    "RTX 3060": [
+        "NVIDIA RTX 4060",
+        "NVIDIA RTX 5060",
+        "NVIDIA RTX 5060 Ti"
     ],
 
-    "NVIDIA RTX 4070": [
-        "NVIDIA RTX 5070 Ti",
+    "RX 6600": [
+        "AMD RX 7600",
+        "AMD RX 9060 XT",
+        "NVIDIA RTX 5060"
+    ],
+
+    "RTX 4090": [
+        "NVIDIA RTX 5090",
+        "NVIDIA RTX 5080",
         "AMD RX 9070 XT"
     ]
-
 }
