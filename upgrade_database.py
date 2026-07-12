199e4cad15641e2a004ab2921d80810090bd11ac
Thomas Smith <nexuruscs@gmail.com>
2026-07-12 02:16:43 -0400

    update upgrade recommendations

--- upgrade_database.py
+++ upgrade_database.py
@@ -63,61 +63,45 @@ cpu_upgrades = {
 }
 
 gpu_upgrades = {
+
     "RTX 3060": [
-        "NVIDIA RTX 4060",
-        "NVIDIA RTX 5060",
-        "NVIDIA RTX 5060 Ti"
-    ],
-
-    "RTX 4060": [
         "NVIDIA RTX 5070",
         "NVIDIA RTX 5070 Ti",
         "AMD RX 9070 XT"
     ],
 
-    "RTX 4060 Ti": [
+    "RTX 3060 Ti": [
         "NVIDIA RTX 5070",
         "NVIDIA RTX 5070 Ti",
-        "AMD RX 9070 XT"
+        "AMD RX 9070"
+    ],
+
+    "RTX 3070": [
+        "NVIDIA RTX 5070",
+        "NVIDIA RTX 5070 Ti",
+        "NVIDIA RTX 5080"
+    ],
+
+    "RTX 3080": [
+        "NVIDIA RTX 5070 Ti",
+        "NVIDIA RTX 5080",
+        "NVIDIA RTX 5090"
     ],
 
     "RTX 4070": [
         "NVIDIA RTX 5070",
         "NVIDIA RTX 5070 Ti",
-        "NVIDIA RTX 5080"
-    ],
-
-    "RTX 4070 SUPER": [
-        "NVIDIA RTX 5070 Ti",
-        "NVIDIA RTX 5080",
         "AMD RX 9070 XT"
     ],
 
     "RTX 4070 Ti": [
-        "NVIDIA RTX 5070 Ti",
         "NVIDIA RTX 5080",
         "NVIDIA RTX 5090"
-    ],
-
-    "RTX 4070 Ti SUPER": [
-        "NVIDIA RTX 5080",
-        "NVIDIA RTX 5090",
-        "AMD RX 9070 XT"
     ],
 
     "RTX 4080": [
         "NVIDIA RTX 5080",
         "NVIDIA RTX 5090"
-    ],
-
-    "RTX 4080 SUPER": [
-        "NVIDIA RTX 5080",
-        "NVIDIA RTX 5090"
-    ],
-
-    "RTX 4090": [
-        "NVIDIA RTX 5090",
-        "NVIDIA RTX 5080"
     ],
 
     "RX 6600": [
@@ -132,21 +116,10 @@ gpu_upgrades = {
         "NVIDIA RTX 5070"
     ],
 
-    "RX 6800": [
-        "AMD RX 7800 XT",
-        "AMD RX 9070 XT",
-        "NVIDIA RTX 5070"
-    ],
-
-    "RX 7700 XT": [
-        "AMD RX 7800 XT",
-        "AMD RX 9070",
-        "NVIDIA RTX 5070"
-    ],
-
     "RX 7800 XT": [
         "AMD RX 9070 XT",
         "NVIDIA RTX 5070 Ti",
         "NVIDIA RTX 5080"
     ]
+
 }
